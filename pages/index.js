16215ecc8167a5218b6df58a672b1721fe6aad50
Tim Cisneros <tcisneros.cis@gmail.com>
2021-02-02 13:01:39 -0800

    fixed folder name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-import Navbar from '../Components/Navbar';
-import Main from '../Components/Main';
+import Navbar from '../components/Navbar';
+import Main from '../components/Main';
 
 export default function Home() {
     const [category, setCategory] = useState('Top U.S. Headlines');
