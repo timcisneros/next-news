55ca3f1e7afa2df5038e4df6eeff99c3c70eaf73
Tim Cisneros <tcisneros.cis@gmail.com>
2021-02-02 13:45:22 -0800

    fixed fetch link to secure protocol

--- pages/api/newsapi.js
+++ pages/api/newsapi.js
@@ -2,7 +2,7 @@ export const fetchArticles = async (key) => {
     const apiKey = process.env.NEXT_PUBLIC_NEWSID;
 
     const c = key.queryKey[1];
-    const urlTop = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+    const urlTop = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
     const urlSearch = `https://newsapi.org/v2/everything?q=${c}&apiKey=${apiKey}`;
 
     const url = c === 'Top U.S. Headlines' ? urlTop : urlSearch;
